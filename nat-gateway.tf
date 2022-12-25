87330f29b6f7e3ea2aac5b05fe1424c8eac8117b
Alex Simisi Nta <alexsms.devops@gmail.com>
2022-12-25 01:39:49 +0100

    formating file

--- nat-gateway.tf
+++ nat-gateway.tf
@@ -22,7 +22,7 @@ resource "aws_eip" "eip-for-nat-gateway-2" {
 # terraform create aws nat gateway
 resource "aws_nat_gateway" "nat_gateway-1" {
   allocation_id = aws_eip.eip-for-nat-gateway-1.id
-  subnet_id = aws_subnet.public-subnet-1.id
+  subnet_id     = aws_subnet.public-subnet-1.id
 
   tags = {
     Name = "Nat Gateway Public Subnet 1"
@@ -33,7 +33,7 @@ resource "aws_nat_gateway" "nat_gateway-1" {
 # terraform create aws nat gateway
 resource "aws_nat_gateway" "nat_gateway-2" {
   allocation_id = aws_eip.eip-for-nat-gateway-2.id
-  subnet_id = aws_subnet.public-subnet-2.id
+  subnet_id     = aws_subnet.public-subnet-2.id
 
   tags = {
     Name = "Nat Gateway Public Subnet 2"
@@ -46,7 +46,7 @@ resource "aws_route_table" "private-route-table-1" {
   vpc_id = aws_vpc.vpc.id
 
   route {
-    cidr_block = "0.0.0.0/0"
+    cidr_block     = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.nat_gateway-1.id
   }
 
@@ -58,14 +58,14 @@ resource "aws_route_table" "private-route-table-1" {
 ## Associate Private Subnet 1 with "Private Route Table 1"
 # terraform aws associate subnet with route table
 resource "aws_route_table_association" "private-subnet-1-route-table-association" {
-  subnet_id = aws_subnet.private-subnet-1.id
+  subnet_id      = aws_subnet.private-subnet-1.id
   route_table_id = aws_route_table.private-route-table-1.id
 }
 
 ## Associate Private Subnet 3 with "Private Route Table 1"
 # terraform aws associate subnet with route table
 resource "aws_route_table_association" "private-subnet-3-route-table-association" {
-  subnet_id = aws_subnet.private-subnet-3.id
+  subnet_id      = aws_subnet.private-subnet-3.id
   route_table_id = aws_route_table.private-route-table-1.id
 }
 
@@ -75,7 +75,7 @@ resource "aws_route_table" "private-route-table-2" {
   vpc_id = aws_vpc.vpc.id
 
   route {
-    cidr_block = "0.0.0.0/0"
+    cidr_block     = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.nat_gateway-2.id
   }
 
@@ -87,13 +87,13 @@ resource "aws_route_table" "private-route-table-2" {
 ## Associate Private Subnet 2 with "Private Route Table 2"
 # terraform aws associate subnet with route table
 resource "aws_route_table_association" "private-subnet-2-route-table-association" {
-  subnet_id = aws_subnet.private-subnet-2.id
+  subnet_id      = aws_subnet.private-subnet-2.id
   route_table_id = aws_route_table.private-route-table-2.id
 }
 
 ## Associate Private Subnet 4 with "Private Route Table 2"
 # terraform aws associate subnet with route table
 resource "aws_route_table_association" "private-subnet-4-route-table-association" {
-  subnet_id = aws_subnet.private-subnet-4.id
+  subnet_id      = aws_subnet.private-subnet-4.id
   route_table_id = aws_route_table.private-route-table-2.id
 }
--- variables.tf
+++ variables.tf
@@ -1,47 +1,47 @@
 # project name
 variable "project-name" {
   description = "The name of the project"
-  type = string
+  type        = string
 }
 
 # vpc
 variable "vpv-cidr" {
   description = "the cidr of vpc"
-  type = string
+  type        = string
 }
 
 # public subnet 1
 variable "public-subnet-1-cidr" {
   default = "the cidr of public subnet 1"
-  type = string
+  type    = string
 }
 
 # public subnet 2
 variable "public-subnet-2-cidr" {
   default = "the cidr of public subnet 2"
-  type = string
+  type    = string
 }
 
 # private subnet 1
 variable "private-subnet-1-cidr" {
   default = "the cidr of public subnet 1"
-  type = string
+  type    = string
 }
 
 # private subnet 2
 variable "private-subnet-2-cidr" {
   default = "the cidr of public subnet 2"
-  type = string
+  type    = string
 }
 
 # private subnet 3
 variable "private-subnet-3-cidr" {
   default = "the cidr of public subnet 3"
-  type = string
+  type    = string
 }
 
 # private subnet 4
 variable "private-subnet-4-cidr" {
   default = "the cidr of public subnet 4"
-  type = string
+  type    = string
 }
