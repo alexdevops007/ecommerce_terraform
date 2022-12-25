30f90b1c8b556bc8158cd9316291fe2c7c1fa09f
Alex Simisi Nta <alexsms.devops@gmail.com>
2022-12-25 03:12:47 +0100

    create security group for the application Load Balancer

---
+++ security-group.tf
@@ -0,0 +1,34 @@
+## Create Security Group for the Application Load Balancer
+# terraform aws create security group
+resource "aws_security_group" "alb-security-group" {
+  name = "ALB Security Group"
+  description = "Enable HTTP/HTTPS access on Port 80/443"
+  vpc_id = aws_vpc.vpc.id
+
+  ingress {
+    description      = "HTTP Access"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "HTTPS Access"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ALB Security Group"
+  }
+}
