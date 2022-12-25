9b811bf7b4631856e2ca1b1dfb8fbf3aba2f2a5f
Alex Simisi Nta <alexsms.devops@gmail.com>
2022-12-25 05:16:27 +0100

    create a webserver security group

--- security-group.tf
+++ security-group.tf
@@ -59,3 +59,46 @@ resource "aws_security_group" "ssh-security-group" {
     Name = "SSH Security Group"
   }
 }
+
+## Create Security Group for the Web Server
+# terraform aws create security group
+resource "aws_security_group" "webserver-security-group" {
+  name = "WEB-SERVER Security Group"
+  description = "Enable HTTP/HTTPS access on Port 80/443 via ALB and SSH on Port 22 via SSH SG"
+  vpc_id = aws_vpc.vpc.id
+
+  ingress {
+    description      = "HTTP Access"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    security_groups = ["${aws_security_group.alb-security-group.id}"]
+  }
+
+  ingress {
+    description      = "HTTPS Access"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    security_groups = ["${aws_security_group.alb-security-group.id}"]
+  }
+
+  ingress {
+    description      = "SSH Access"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    security_groups = ["${aws_security_group.ssh-security-group.id}"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "WEB-SERVER Security Group"
+  }
+}
