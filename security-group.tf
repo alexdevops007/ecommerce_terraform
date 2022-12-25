2426e7f73b73b0780992689a067ff9a550b8ebb7
Alex Simisi Nta <alexsms.devops@gmail.com>
2022-12-25 05:24:08 +0100

    create database security group

--- security-group.tf
+++ security-group.tf
@@ -102,3 +102,30 @@ resource "aws_security_group" "webserver-security-group" {
     Name = "WEB-SERVER Security Group"
   }
 }
+
+## Create Security Group for the Database
+# terraform aws create security group
+resource "aws_security_group" "database-security-group" {
+  name = "Database Security Group"
+  description = "Enable MySQL/Aurora access on Port 3306"
+  vpc_id = aws_vpc.vpc.id
+
+  ingress {
+    description      = "MySQL/Aurora Access"
+    from_port        = 3306
+    to_port          = 3306
+    protocol         = "tcp"
+    security_groups = ["${aws_security_group.webserver-security-group.id}"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Database Security Group"
+  }
+}
